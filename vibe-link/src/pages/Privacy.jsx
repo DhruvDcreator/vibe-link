f3e610c71144e2e77978671f7142df8eadf05f19
Dhruv <dhruvdhanuka28@gmail.com>
2026-05-18 05:15:16 +0530

    try otp

--- vibe-link/src/pages/Privacy.jsx
+++ vibe-link/src/pages/Privacy.jsx
@@ -82,7 +82,7 @@ export default function Privacy({
                 </p>
 
                 <p>
-                  Last Updated: May 17, 2026
+                  Last Updated: May 18, 2026
                 </p>
 
               </section>
