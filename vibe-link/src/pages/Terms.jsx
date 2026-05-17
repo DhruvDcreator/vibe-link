8cbbe10225834e9fdb6259c4ac0e4aa4eaae471f
Dhruv <dhruvdhanuka28@gmail.com>
2026-05-18 05:18:39 +0530

    verify

--- vibe-link/src/pages/Terms.jsx
+++ vibe-link/src/pages/Terms.jsx
@@ -82,7 +82,7 @@ export default function Terms({
                 </p>
 
                 <p>
-                  Last Updated: May 16, 2026
+                  Last Updated: May 17, 2026
                 </p>
 
               </section>
