08b2590ab611197fa2c0b5339d3a7411058b13cd
Dhruv <dhruvdhanuka28@gmail.com>
2026-05-18 05:17:39 +0530

    keep trying

--- vibe-link/src/pages/Terms.jsx
+++ vibe-link/src/pages/Terms.jsx
@@ -82,7 +82,7 @@ export default function Terms({
                 </p>
 
                 <p>
-                  Last Updated: May 15, 2026
+                  Last Updated: May 16, 2026
                 </p>
 
               </section>
