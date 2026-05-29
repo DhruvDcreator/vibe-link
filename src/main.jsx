80b1420f843783e9393400736a965892427b9bfb
Dhruv <dhruvdhanuka28@gmail.com>
2026-05-29 19:37:24 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <div
+    style={{
+      background: "#03040A",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "40px",
+    }}
+  >
+    SAFARI TEST
+  </div>
+);
