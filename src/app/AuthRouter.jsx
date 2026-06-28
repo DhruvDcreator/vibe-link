8605d0b4b85d342db4e8a840f0c341270f0b4d77
Dhruv <dhruvdhanuka28@gmail.com>
2026-06-28 16:26:27 +0530

    login

--- src/app/AuthRouter.jsx
+++ src/app/AuthRouter.jsx
@@ -19,6 +19,111 @@ export default function AuthRouter({
   if (screen === "welcome") {
     return <Welcome setScreen={setScreen} />;
   }
+  if (
+    screen ===
+    "auth"
+  ) {
+    return (
+      <Auth
+        setScreen={
+          setScreen
+        }
+        isLogin={
+          screen === "auth"
+        }
+        setIsLogin={(value) => {
+          if (value) {
+            setScreen("auth");
+          } else {
+            setScreen("authSignup");
+          }
+        }}
+        username={
+          username
+        }
+        setUsername={
+          setUsername
+        }
+        email={
+          email
+        }
+        setEmail={
+          setEmail
+        }
+        password={
+          password
+        }
+        setPassword={
+          setPassword
+        }
+        age={
+          age
+        }
+        setAge={
+          setAge
+        }
+        phone={
+          phone
+        }
+        setPhone={
+          setPhone
+        }
+        showPassword={
+          showPassword
+        }
+        setShowPassword={
+          setShowPassword
+        }
+        loginUser={loginUser}
+        sendOtp={async () => {
+          try {
+            const otp =
+              Math.floor(
+                1000 +
+                Math.random() *
+                9000
+              ).toString();
+
+            setGeneratedOtp(
+              otp
+            );
+
+            const response =
+              await emailjs.send(
+                "service_6xy30ia",
+                "template_0dyw2wl",
+                {
+                  to_email:
+                    email,
+                  otp:
+                    otp,
+                },
+                "agiLAySvLuJA74eKO"
+              );
+
+            console.log(
+              "OTP SENT",
+              response
+            );
+
+            setScreen(
+              "otp"
+            );
+          } catch (error) {
+            console.log(
+              "EMAILJS ERROR:",
+              error
+            );
+
+            alert(
+              "Failed to send OTP"
+            );
+          }
+        }}
+      />
+    );
+  }
+
 if (
     screen ===
     "forgotPassword"
